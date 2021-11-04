167266324d0c5377a3ccffd25d98138b260c4d86
Mary Nash <MNash@DCCUWEB.LOCAL>
2021-11-04 17:06:14 -0400

    added display list

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from "../hero";
+import { HEROES } from "../mock-heroes";
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes= HEROES;
+
+  
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor() { }
+
+  //ngOnIt is a lifecycle hook.  Angular calls ngOnIt shortly after creating a component
+  //it's a good place to put initialization logic
+  ngOnInit(): void {
+  }
+
+ 
+
+}
